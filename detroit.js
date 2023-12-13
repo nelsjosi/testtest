1f08bbfefb562b14e94e20fa18f9ca0f9a9d136e
nelsjosi <146250827+nelsjosi@users.noreply.github.com>
2023-12-12 18:19:05 -0800

    Update detroit.js

--- detroit.js
+++ detroit.js
@@ -1,16 +1,17 @@
 var map = L.map('map').setView([42.3314, -83.0458], 10);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/nelsjosi/clpwvdxqf008n01mr803hd38m/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmVsc2pvc2kiLCJhIjoiY2xvdXJpcTlyMGlnbDJqbzZnYzBpaDJwdyJ9.TFehieFMapa9HP0UJgkETg', {
+L.tileLayer('https://api.mapbox.com/styles/v1/nelsjosi/clq1pq8wh00m401re3kqifpn5/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmVsc2pvc2kiLCJhIjoiY2xvdXJpcTlyMGlnbDJqbzZnYzBpaDJwdyJ9.TFehieFMapa9HP0UJgkETg', {
     tileSize: 512,
     zoomOffset: -1,
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
 // Function to get color based on ethnicity
 var getColor = function (feature) {
     if (!feature.properties.B03002_001) {
-        return '#808080'; // Gray for no data
+        return 'rgb(169,169,169)'; // Gray for no data
     }
 
     var blackPercentage = (feature.properties.AA / feature.properties.B03002_001) * 100;
@@ -18,15 +19,44 @@ var getColor = function (feature) {
     var hispanicPercentage = (feature.properties.Hispanic / feature.properties.B03002_001) * 100;
 
     if (blackPercentage > whitePercentage && blackPercentage > hispanicPercentage) {
-        return '#FA8072'; // Salmon pink for majority black
+        return 'rgb(255,215,0)'; // Gold for majority black
     } else if (hispanicPercentage > whitePercentage && hispanicPercentage > blackPercentage) {
-        return '#800080'; // Royal purple for majority hispanic
+        return 'rgb(255,99,71)'; // Tomato red for majority hispanic
     } else if (whitePercentage > blackPercentage && whitePercentage > hispanicPercentage) {
-        return '#32127A'; // Dark purple for majority white
+        return 'rgb(50,205,50)'; // Lime green for majority white
     } else {
-        return '#808080'; // Gray for no majority ethnicity
+        return 'rgb(169,169,169)'; // Gray for no majority ethnicity
     }
 };
+
+// Adjusted colors for color blindness
+var gold = 'rgb(255,215,0)';
+var tomatoRed = 'rgb(255,99,71)';
+var limeGreen = 'rgb(50,205,50)';
+
+var getColorForColorBlind = function (feature) {
+    if (!feature.properties.B03002_001) {
+        return 'rgb(169,169,169)'; // Gray for no data
+    }
+
+    var blackPercentage = (feature.properties.AA / feature.properties.B03002_001) * 100;
+    var whitePercentage = (feature.properties.White / feature.properties.B03002_001) * 100;
+    var hispanicPercentage = (feature.properties.Hispanic / feature.properties.B03002_001) * 100;
+
+    if (blackPercentage > whitePercentage && blackPercentage > hispanicPercentage) {
+        return gold;
+    } else if (hispanicPercentage > whitePercentage && hispanicPercentage > blackPercentage) {
+        return tomatoRed;
+    } else if (whitePercentage > blackPercentage && whitePercentage > hispanicPercentage) {
+        return limeGreen;
+    } else {
+        return 'rgb(169,169,169)'; // Gray for no majority ethnicity
+    }
+};
+
+
+
+
 
 var selectedLayer = null;
 
@@ -59,22 +89,32 @@ function addGeoJsonLayer(url, colorProperty, isFacility) {
                     }
                 },
                 onEachFeature: function (feature, layer) {
-                    if (!isFacility) {
+                    if (isFacility) {
+                        // Display only the waste facility data
+                        layer.bindPopup(`<b>${feature.properties.SiteSpecificName}</b><br>${feature.properties.Address}, ${feature.properties.City}, ${feature.properties.County}`);
+                    } else {
+                        // Display only the top 3 ethnicities
+                        var topEthnicities = getTopEthnicities(feature.properties);
                         var popupContent = `<b>${feature.properties.RaceEthnic}</b><br>`;
-                        for (var key in feature.properties) {
-                            popupContent += `<b>${key}:</b> ${feature.properties[key]}<br>`;
-                        }
-                        layer.bindPopup(popupContent);
-                        layer.on({
-                            click: function (e) {
-                                if (selectedLayer) {
-                                    resetHighlight(selectedLayer);
-                                }
-                                highlightFeature(e);
-                                selectedLayer = e.target;
-                            }
+                        topEthnicities.forEach(ethnicity => {
+                            popupContent += `<b>${ethnicity.label}:</b> ${ethnicity.value}<br>`;
                         });
+
+                        // Change "Go to Form" button text and link
+                        popupContent += `<br><button onclick="openForm('${feature.id}')"><a href="https://www.surveymonkey.com/r/W5ZGBBJ" target="_blank">Go to Community Impact Survey Form</a></button>`;
+
+                        layer.bindPopup(popupContent);
                     }
+
+                    layer.on({
+                        click: function (e) {
+                            if (selectedLayer) {
+                                resetHighlight(selectedLayer);
+                            }
+                            highlightFeature(e);
+                            selectedLayer = e.target;
+                        }
+                    });
                 }
             }).addTo(map);
         });
@@ -100,8 +140,8 @@ var legend = L.control({ position: 'bottomright' });
 
 legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend'),
-        ethnicities = ['No Data', 'Majority Black', 'Majority White', 'Majority Hispanic'],
-        colors = ['#808080', '#FA8072', '#32127A', '#800080'];
+        ethnicities = ['No Data', 'Majority Black', 'Majority White', 'Majority Hispanic or Latino'],
+        colors = ['#AAAAAA', '#FFD700', '#00FF00', '#FF6347'];
 
     for (var i = 0; i < ethnicities.length; i++) {
         div.innerHTML +=
@@ -111,6 +151,8 @@ legend.onAdd = function (map) {
 
     return div;
 };
+
+legend.addTo(map);
 
 legend.addTo(map);
 
@@ -127,6 +169,7 @@ searchControl.onAdd = function (map) {
 searchControl.addTo(map);
 
 // Search functionality
+// Search functionality
 function searchLocation() {
     var searchInput = document.getElementById('search-input').value;
     var searchUrl = 'https://nominatim.openstreetmap.org/search?format=json&q=' + searchInput;
@@ -136,7 +179,10 @@ function searchLocation() {
         .then(data => {
             if (data && data.length > 0) {
                 var latlng = [data[0].lat, data[0].lon];
-                map.panTo(latlng);
+                
+                // Set the zoom level and pan to the searched location
+                map.setView(latlng, 14);
+
                 if (selectedLayer) {
                     resetHighlight(selectedLayer);
                 }
@@ -147,6 +193,7 @@ function searchLocation() {
         })
         .catch(error => console.error('Error searching location:', error));
 }
+
 
 // Function to reset highlight
 function resetHighlight(layer) {
@@ -173,4 +220,20 @@ function highlightFeature(e) {
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
     }
+}
+
+// Function to get the top 3 ethnicities
+function getTopEthnicities(properties) {
+    var ethnicityData = [
+        { label: 'White', value: properties.White },
+        { label: 'Black', value: properties.AA },
+        { label: 'Hispanic', value: properties.Hispanic }
+        // Add more ethnicities as needed
+    ];
+
+    // Sort by value in descending order
+    ethnicityData.sort((a, b) => b.value - a.value);
+
+    // Return the top 3 ethnicities
+    return ethnicityData.slice(0, 3);
 }
